b548fa1fdef1decb8c50bc44278e1ce36b8663d0
rogerk <rogerk@localhost>
2014-01-26 16:41:57 -0600

    Release version 2.14.126.467

---
+++ src/Library/Library.Test/TestCrc64.cs
@@ -0,0 +1,172 @@
+﻿#region Copyright 2011-2014 by Roger Knapp, Licensed under the Apache License, Version 2.0
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#endregion
+using System.Text;
+using CSharpTest.Net.IO;
+using NUnit.Framework;
+
+namespace CSharpTest.Net.Library.Test
+{
+    [TestFixture]
+    public class TestCrc64
+    {
+        private static void AssertCrc64(string input, ulong expected)
+        {
+            //Can not use ASCII encoding since it does not support 8-bit characters: resumé
+            byte[] bytes = new byte[input.Length];
+            for (int i = 0; i < input.Length; i++)
+                bytes[i] = unchecked((byte)input[i]);
+            AssertCrc64(bytes, expected);
+        }
+        private static void AssertCrc64(byte[] input, ulong expectedu)
+        {
+            long expected = unchecked((long) expectedu);
+            Crc64 crc = new Crc64(input);
+            Assert.AreEqual(expected, crc.Value);
+
+            crc = new Crc64(0);
+            crc.Add(input);
+            Assert.AreEqual(expected, crc.Value);
+
+            crc = new Crc64();
+            crc.Add(input, 0, input.Length);
+            Assert.AreEqual(expected, crc.Value);
+
+            crc = new Crc64();
+            foreach (byte b in input)
+                crc.Add(b);
+            Assert.AreEqual(expected, crc.Value);
+        }
+
+        [Test]
+        public void TestKnown123456789()
+        { AssertCrc64("123456789", 0x995dc9bbdf1939faul); }
+
+        [Test]
+        public void TestKnownResume1()
+        { AssertCrc64("resume", 0xd68a044c92bb8a32ul); }
+
+        [Test]
+        public void TestKnownResume2()
+        {
+            //the string is "resumé" with the accented 'é'; however, I did not want to depend on proper text encoding.
+            AssertCrc64("resum" + (char)233, 0x9397175445ad6725ul); 
+        }
+
+        [Test]
+        public void TestKnownBytesLeadingZero()
+        { AssertCrc64(new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x28, 0x86, 0x4d, 0x7f, 0x99 }, 0x578b800a3b157c37ul); }
+
+        [Test]
+        public void TestKnownBytesLeading0Xff()
+        { AssertCrc64(new byte[] { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x00, 0x00, 0x00, 0x28, 0xc5, 0x5e, 0x45, 0x7a }, 0xaafb709ed67a8193ul); }
+
+        [Test]
+        public void TestKnownBytesSequence()
+        { AssertCrc64(Encoding.UTF8.GetBytes("TEST"), 0xe10c672b18cfcdd0ul); }
+
+        [Test]
+        public void TestOperatorEquality()
+        {
+            Crc64 empty = new Crc64();
+            Crc64 value = new Crc64("Hello");
+            Crc64 copy = new Crc64(value.Value);
+
+            Assert.IsTrue(value == copy);
+            Assert.IsFalse(value == empty);
+            Assert.IsTrue(value == copy.Value);
+            Assert.IsFalse(value == empty.Value);
+
+            Assert.IsFalse(value != copy);
+            Assert.IsTrue(value != empty);
+            Assert.IsFalse(value != copy.Value);
+            Assert.IsTrue(value != empty.Value);
+        }
+        [Test]
+        public void TestCrc64Equals()
+        {
+            Crc64 empty = new Crc64();
+            Crc64 value = new Crc64("Hello");
+            Crc64 copy = new Crc64(value.Value);
+
+            Assert.IsTrue(value.Equals(copy));
+            Assert.IsFalse(value.Equals(empty));
+            Assert.IsTrue(value.Equals(copy.Value));
+            Assert.IsFalse(value.Equals(empty.Value));
+
+            Assert.IsTrue(value.Equals((object)copy));
+            Assert.IsFalse(value.Equals((object)empty));
+            Assert.IsTrue(value.Equals((object)copy.Value));
+            Assert.IsFalse(value.Equals((object)empty.Value));
+        }
+
+        [Test]
+        public void TestHashValue()
+        {
+            Crc64 crc = new Crc64();
+            Assert.AreEqual(0, crc.Value);
+            Assert.AreEqual(0, crc.GetHashCode());
+
+            crc.Add(0x1b);
+
+            Assert.AreNotEqual(0, crc.Value);
+            Assert.AreEqual((int)crc.Value ^ (int)(crc.Value >> 32), crc.GetHashCode());
+        }
+
+        [Test]
+        public void TestOperatorPlusBytes()
+        {
+            Crc64 all = new Crc64(new byte[] { 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8 });
+            Crc64 crc = new Crc64();
+            Assert.AreEqual(0, crc.Value);
+            crc += new byte[] { 0x1, 0x2, 0x3, 0x4 };
+            crc += 0x5;
+            crc += 0x6;
+            crc += 0x7;
+            crc += 0x8;
+            Assert.AreEqual(all.Value, crc.Value);
+        }
+
+        [Test]
+        public void TestOperatorPlusString()
+        {
+            Crc64 all = new Crc64("hello there world");
+            Crc64 crc = new Crc64();
+            Assert.AreEqual(0, crc.Value);
+            crc += "hello ";
+            crc += "there ";
+            crc += "world";
+            Assert.AreEqual(all.Value, crc.Value);
+        }
+
+        [Test]
+        public void TestAddByteRange()
+        {
+            Crc64 all = new Crc64(new byte[] { 0x2, 0x3, 0x4, 0x5, 0x6 });
+            Crc64 crc = new Crc64();
+            Assert.AreEqual(0, crc.Value);
+            crc.Add(new byte[] { 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8 }, 1, 5);
+            Assert.AreEqual(all.Value, crc.Value);
+        }
+
+        [Test]
+        public void TestToString()
+        {
+            Assert.AreEqual("0000000000000000", new Crc64().ToString());
+            Assert.AreEqual("0000000000100100", new Crc64(0x00100100).ToString());
+            Assert.AreEqual("FFFFFFFFF0100100", new Crc64(unchecked((int)0xF0100100)).ToString());
+            Assert.AreEqual("F010010011100110", new Crc64(unchecked((long)0xF010010011100110ul)).ToString());
+        }
+    }
+}
---
+++ src/Library/Library.Test/TestDbGuid.cs
@@ -0,0 +1,256 @@
+﻿#region Copyright 2012 by Roger Knapp, Licensed under the Apache License, Version 2.0
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using CSharpTest.Net.Data;
+using NUnit.Framework;
+
+namespace CSharpTest.Net.Library.Test
+{
+    [TestFixture]
+    public class TestDbGuid
+    {
+        [Test]
+        public void GuidCompare()
+        {
+            DbGuid a = DbGuid.NewGuid();
+            DbGuid b = DbGuid.NewGuid();
+            Assert.AreEqual(-1, a.CompareTo(b));
+            Assert.AreEqual(-1, a.ToGuid().CompareTo(b.ToGuid()));
+        }
+
+        [Test]
+        public void TestRoundTripGuidToDbGuid()
+        {
+            Guid testA = Guid.NewGuid();
+            DbGuid dbtest = (DbGuid)testA;
+            Guid testB = dbtest.ToGuid();
+            Assert.AreEqual(testA, testB);
+        }
+
+        [Test]
+        public void TestRoundTripDbGuidToGuid()
+        {
+            DbGuid testA = DbGuid.NewGuid();
+            Guid dbtest = testA;
+            DbGuid testB = (DbGuid)dbtest;
+            Assert.AreEqual(testA, testB);
+        }
+
+        [Test]
+        public void TestRoundTripBytes()
+        {
+            DbGuid testA = DbGuid.NewGuid();
+            byte[] bytes = testA.ToByteArray();
+            DbGuid testB = new DbGuid(bytes);
+            Assert.AreEqual(testA, testB);
+        }
+
+        [Test]
+        public void TestRoundTripByteOffset()
+        {
+            DbGuid testA = DbGuid.NewGuid();
+            byte[] bytes = new byte[1024];
+            testA.ToByteArray(bytes, 512);
+            DbGuid testB = new DbGuid(bytes, 512);
+            Assert.AreEqual(testA, testB);
+        }
+
+        [Test]
+        public void TestRoundTrip64Bit()
+        {
+            DbGuid testA = DbGuid.NewGuid();
+            DbGuid testB = new DbGuid(testA.High64, testA.Low64);
+            Assert.AreEqual(testA, testB);
+        }
+
+        [Test]
+        public void TestDiscreteCtor()
+        {
+            DbGuid testA = new DbGuid(0x01020304, 0x0506, 0x0708, 0x10, 0x20, 0x30, 0x40, 0x50, 0x60, 0x70, 0x80);
+            Guid testB = new Guid(0x01020304, 0x0506, 0x0708, 0x10, 0x20, 0x30, 0x40, 0x50, 0x60, 0x70, 0x80);
+            Assert.AreEqual(testB, testA.ToGuid());
+        }
+
+        [Test]
+        public void TestEmpty()
+        {
+            Assert.AreEqual(Guid.Empty, DbGuid.Empty.ToGuid());
+            Assert.AreEqual(DbGuid.Empty, new DbGuid(Guid.Empty));
+            Assert.AreEqual(Guid.Empty, new DbGuid().ToGuid());
+            Assert.AreEqual(DbGuid.Empty, new DbGuid());
+            Assert.AreEqual(DbGuid.Empty, new DbGuid(0, 0));
+            Assert.AreEqual(0L, DbGuid.Empty.High64);
+            Assert.AreEqual(0L, DbGuid.Empty.Low64);
+        }
+
+        [Test]
+        public void TestDateTime()
+        {
+            // Note: DateTime precision is +/- 1ms
+            DateTime start = DateTime.UtcNow.AddMilliseconds(-1);
+            DbGuid test = DbGuid.NewGuid();
+            Assert.IsTrue(test.ToDateTimeUtc() > start, "expected {0} > {1}", test.ToDateTimeUtc(), start);
+            DateTime now = DateTime.UtcNow.AddMilliseconds(1);
+            Assert.IsTrue(test.ToDateTimeUtc() <= now, "expected {0} <= {1}", test.ToDateTimeUtc(), now);
+        }
+
+        [Test]
+        public void TestRoundTripSqlGuid()
+        {
+            DbGuid testA = DbGuid.NewGuid();
+            DbGuid dbtest = testA.ToSqlGuid();
+            Assert.AreEqual(dbtest, testA);
+            Assert.AreEqual(dbtest.GetHashCode(), testA.GetHashCode());
+            Assert.AreEqual(0, dbtest.CompareTo(testA));
+            DbGuid testB = dbtest.ToSequenceGuid();
+            Assert.AreEqual(testA, testB);
+        }
+
+        [Test]
+        public void TestToString()
+        {
+            Guid test = Guid.NewGuid();
+            Assert.AreEqual(test.ToString(), new DbGuid(test).ToString());
+            Assert.AreEqual(test.ToString("N"), new DbGuid(test).ToString("N"));
+            Assert.AreEqual(test.ToString("B", CultureInfo.InvariantCulture), new DbGuid(test).ToString("B", CultureInfo.InvariantCulture));
+        }
+
+        [Test]
+        public void TestSerializer()
+        {
+            DbGuid[] ids = new DbGuid[16];
+            MemoryStream stream = new MemoryStream();
+            for (int i = 0; i < ids.Length; i++)
+            {
+                ids[i] = DbGuid.NewGuid();
+                DbGuid.Serializer.WriteTo(ids[i], stream);
+            }
+            stream.Position = 0;
+            for (int i = 0; i < ids.Length; i++)
+            {
+                DbGuid test = DbGuid.Serializer.ReadFrom(stream);
+                Assert.AreEqual(ids[i], test);
+            }
+            Assert.AreEqual(-1, stream.ReadByte());
+        }
+
+        [Test, Explicit]
+        public void TestGenerationPerf()
+        {
+            var timer = Stopwatch.StartNew();
+            int total = 0, target = 0;
+            while (timer.ElapsedMilliseconds < 1000)
+            {
+                for (target += 1000000; total < target; total++)
+                {
+                    DbGuid.NewGuid();
+                }
+            }
+            timer.Stop();
+            Console.WriteLine("Generated {0:n0} guids in {1:n2} seconds, averaging {2:n2} per second.",
+                total, timer.Elapsed.TotalSeconds, total / timer.Elapsed.TotalSeconds);
+        }
+
+        [Test]
+        public void TestSequence()
+        {
+            DbGuid last = DbGuid.NewGuid();
+            for (int i = 0; i < 10000; i++)
+            {
+                DbGuid test = DbGuid.NewGuid();
+                TestSequence(last, test);
+                TestSequence(last, test.ToSqlGuid());
+                TestSequence(last.ToSqlGuid(), test);
+                TestSequence(last.ToSqlGuid(), test.ToSqlGuid());
+                Assert.IsTrue(last.CompareTo(test) < 0);
+                last = test;
+            }
+        }
+
+        private void TestSequence(DbGuid small, DbGuid big)
+        {
+            Assert.IsTrue(small != big);
+            Assert.IsTrue(big != small);
+
+            Assert.IsTrue(small < big);
+            Assert.IsTrue(big > small);
+
+            Assert.IsTrue(small <= big);
+            Assert.IsTrue(big >= small);
+
+            Assert.IsFalse(small == big);
+            Assert.IsFalse(big == small);
+
+            Assert.IsFalse(big < small);
+            Assert.IsFalse(small > big);
+            
+            Assert.IsFalse(big <= small);
+            Assert.IsFalse(small >= big);
+            
+            Assert.IsTrue(small.CompareTo(big) < 0);
+            Assert.IsTrue(big.CompareTo(small) > 0);
+
+            Assert.IsTrue(DbGuid.Comparer.Compare(small, big) < 0);
+            Assert.IsTrue(DbGuid.Comparer.Compare(big, small) > 0);
+            
+            Assert.IsTrue(small.ToString().CompareTo(big.ToString()) < 0);
+        }
+
+        [Test]
+        public void TestEquality()
+        {
+            DbGuid testA = DbGuid.NewGuid();
+            TestEquality(testA, testA, true);
+            TestEquality(testA, DbGuid.NewGuid(), false);
+            TestEquality(testA, new DbGuid(Guid.NewGuid()), false);
+            TestEquality(testA, testA.ToSqlGuid(), true);
+            TestEquality(testA, testA.ToSqlGuid().ToSequenceGuid(), true);
+        }
+
+        private void TestEquality(DbGuid testA, DbGuid testB, bool expect)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                Assert.AreEqual(expect, testA.GetHashCode() == testB.GetHashCode());
+                Assert.AreEqual(expect, DbGuid.Comparer.GetHashCode(testA) == DbGuid.Comparer.GetHashCode(testB));
+
+                Assert.AreEqual(expect, testA == testB);
+                Assert.AreEqual(!expect, testA != testB);
+
+                Assert.AreEqual(expect, ((object) testA).Equals(testB));
+                Assert.AreEqual(expect, ((IEquatable<DbGuid>) testA).Equals(testB));
+
+                Assert.AreEqual(expect, ((IComparable)testA).CompareTo(testB) == 0);
+                Assert.AreEqual(expect, ((IComparable<DbGuid>)testA).CompareTo(testB) == 0);
+
+                Assert.AreEqual(expect, DbGuid.Comparer.Equals(testA, testB));
+                Assert.AreEqual(expect, DbGuid.Comparer.Compare(testA, testB) == 0);
+
+                Assert.AreEqual(expect, testA.ToString() == testB.ToString());
+                Assert.AreEqual(expect, testA.ToString("N") == testB.ToString("N"));
+                Assert.AreEqual(expect, testA.ToString("N", null) == testB.ToString("N", null));
+
+                //swap and try again
+                DbGuid tmp = testA;
+                testA = testB;
+                testB = tmp;
+            }
+        }
+    }
+}
